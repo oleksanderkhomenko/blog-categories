69065983d238c5b91c55fb98267c7bcc33e006e6
Oleksandr Khomenko <khomenko.artistsoft@gmail.com>
2018-01-05 17:48:34 +0200

    added posts crud logic, added styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,49 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[edit update show destroy]
+
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def show
   end
 
-  def delete
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params[:post].permit(:title, :text)
+  end
+
+  def set_post
+    @post = current_user.posts.find(params[:id])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-  belongs_to :article
+  belongs_to :user
 end
